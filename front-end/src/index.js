40b1cfc8385ae79b6cdf17a6f48e8fe51f88a9d8
github username <github email address>
2018-09-11 11:49:00 -0500

    move key

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,22 +1,35 @@
-const clientId = 'FrCY6KN0RweTEp68H5FMrg'
-const apiKey = 'SPEaM7_pa22el3cgbiZUoZ2VuCQRKVG18ypiidtkwxtJ0ipsIbF0YwhO71mXifhqVmD31qLler2IYPypwsD8E1xNNNV4gdC2G2ehTUCGOTc8MLYVJxyX9bIkKACXW3Yx'
-
 document.addEventListener('DOMContentLoaded', function() {
   fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=houston', {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer SPEaM7_pa22el3cgbiZUoZ2VuCQRKVG18ypiidtkwxtJ0ipsIbF0YwhO71mXifhqVmD31qLler2IYPypwsD8E1xNNNV4gdC2G2ehTUCGOTc8MLYVJxyX9bIkKACXW3Yx"
+      "Authorization": `Bearer ${yelpApiKey}`
     }
   })
     .then(res => res.json())
-    .then(console.log)
-})
+    .then(ratingGreaterThanThree)
 
-function  tipGreaterThanZero(data){
-  const x = data.response.venues.filter(venue => {
-    // debugger
-    return venue.stats.tipCount > 0
+function  ratingGreaterThanThree(data){
+  const highRatings = data.businesses.filter(business => {
+    return business.rating > 3
+  })
+  mapTheCoordinates(highRatings)
+}
+
+function mapTheCoordinates(highRatings) {
+  const coordinatesArray = highRatings.map(business => {
+    return business.coordinates
   })
   debugger
 }
+
+
+
+
+
+
+
+
+
+
+})
